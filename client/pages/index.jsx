93fef5f206834deb6a4561bc8f14c98547b30bb6
Rugz007 <dsweb007@gmail.com>
2022-04-21 18:53:34 +0530

    Used swr to fix login issue

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -7,12 +7,7 @@ import { apiData } from "../util/apiData";
 import { fetchData } from "../api/API";
 import { useEffect } from "react";
 export default function Home() {
-  const { data } = useSWR(`${apiData.url}auth/users/me`, fetchData, {
-    refreshInterval: 0,
-  });
-  useEffect(() => {
-  }, [localStorage.getItem("access")])
-  
+  const { data,error } = useSWR(`${apiData.url}auth/users/me`, fetchData,{ refreshInterval: 1000 });
   return (
     <div className={styles.container}>
       <Head>
@@ -28,7 +23,7 @@ export default function Home() {
               <a href="https://pulzion.in">Pulzion</a> MCQ Platform
             </Text>
             <Center>
-              {localStorage && localStorage.getItem("access") ? (
+              {!data ? (
                 <Link href={"/tests"}>
                   <Button w="30%">Go to Tests</Button>
                 </Link>

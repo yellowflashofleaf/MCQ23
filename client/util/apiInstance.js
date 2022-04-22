1791f93d2d0048297b4cb8e7b9616adcbb49a09e
Rugz007 <dsweb007@gmail.com>
2022-04-22 07:52:20 +0530

    Fixed most issues

--- client/util/apiInstance.js
+++ client/util/apiInstance.js
@@ -80,7 +80,6 @@ axiosInstance.interceptors.response.use(
               return axiosInstance(originalRequest);
             })
             .catch((err) => {
-              console.log(err);
             });
         } else if (data.response.status !== 200) {
           localStorage.removeItem(ACCESS_TOKEN);
